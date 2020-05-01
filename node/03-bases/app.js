a94aaf5aa3a696331e114bcb3698b468fcb2cdb1
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-04-30 21:46:29 -0500

    informe 03

--- node/03-bases/app.js
+++ node/03-bases/app.js
@@ -8,24 +8,16 @@ let limite = argv.limite
 
 switch (comando) {
     case 'listar':
-        // console.log('Listado')
         console.log(listarTabla(base, limite))
         break;
-
     case 'crear':
-        // console.log(argv);
         createFile(base, limite)
             .then(msj => console.log(msj))
             .catch(err => console.log(err.message))
         break;
     default:
-
         break;
 }
 
 
-// console.log(argv.base, argv.limite)
 
-
-
-// console.log(argv)
