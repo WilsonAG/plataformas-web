6712929513a68d1cd9e0f81344480598f9217955
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-06-24 11:44:58 -0500

    rest server ready to upload

--- node/07-restserver/server/server.js
+++ node/07-restserver/server/server.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('./config/env');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
 
 app.get('/user', (req, res) => {
     res.json('Get user')
 });
 app.post('/user', (req, res) => {
-    res.json('Post user')
+
+    let body = req.body;
+    if (!body.name ) {
+        return res.status(400).json({ err: true, message: 'El nombre es necesario.'});
+    }
+    res.json({
+        body: req.body
+    })
 });
 app.put('/user/:id', (req, res) => {
     let id = req.params.id;
-    res.json({ 'user': id })
+    let query = req.query.name;
+    res.json({ 'user': id, query: query, params: req.body })
 });
 app.delete('/user', (req, res) => {
     res.json('Delete user')
